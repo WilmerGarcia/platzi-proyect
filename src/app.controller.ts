ae634b897115f6ca4d653b53ae8ac9f015f8aa6c
WilmerGarcia <60454839+WilmerGarcia@users.noreply.github.com>
2023-03-04 15:38:47 -0600

    feat: :construction: first commit

---
+++ src/app.controller.ts
@@ -0,0 +1,32 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('nuevo')
+  newEndpoint() {
+    return 'yo soy nuevo';
+  }
+
+  @Get('/ruta/')
+  hello() {
+    return 'con /sas/';
+  }
+
+  @Get('products/:productId')
+  getProduct(@Param('productId') productId: string) {
+    return `product ${productId}`;
+  }
+
+  @Get('categories/:id/products/:productId')
+  getCategory(@Param('productId') productId: string, @Param('id') id: string) {
+    return `product: ${productId} and categories: ${id}`;
+  }
+}
